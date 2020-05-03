5b282d8f68ebc7e193a42e358ff4252b6142a2fc
Mathew Button <mathewdbutton@gmail.com>
2020-05-03 22:18:28 +1000

    Use graph model more

--- graph.py
+++ graph.py
@@ -1,7 +1,21 @@
 class Graph:
-  def __init__(self, graph_settings):
-    self.settings = graph_settings
-    self.x_axis = self.settings['x-axis']['sets'][0]
-    self.y_axes = self.settings['y-axis']['sets']
-    self.filename = self.settings['name']
-    self.title = self.settings['title']
+  def __init__(self, title="", y_axis=[], x_axis={}, header_row=1, filename="", show=False):
+    self.x_axis = x_axis
+    self.y_axis = y_axis
+    self.filename = filename
+    self.title = title
+    self.header_row = header_row
+    self.show = show
+
+  def y_axis_columns(self):
+    return [set['heading'] for set in self.y_axis["sets"]]
+
+  @staticmethod
+  def from_yaml(data):
+    return Graph(title=data['title'],
+          y_axis=data['y-axis'],
+          x_axis=data['x-axis'],
+          filename=data['name'],
+          header_row=data["header_row"],
+          show=data['show']
+            )
--- graph_plotter.py
+++ graph_plotter.py
@@ -18,23 +18,20 @@ def load_graph_settings(filename="./graph_settings.yml"):
 def generate(data_path, output_path):
     data = load_data(data_path)
     settings = load_graph_settings()
-    for graph in settings['graphs']:
-      graph_model = Graph(graph)
-      x_axis = graph_model.x_axis
+    for graph in [Graph.from_yaml(graph_data) for graph_data in settings['graphs'] ]:
+      x_axis = graph.x_axis['sets'][0]
 
-      y_axis_columns = [set['heading'] for set in graph_model.y_axes ]
-      df = pd.DataFrame(data, columns=[x_axis['heading'], *y_axis_columns])
+      dataFrame = pd.DataFrame(data, columns=[x_axis['heading'], *graph.y_axis_columns()])
 
-      # # Note that even in the OO-style, we use `.pyplot.figure` to create the figure.
-      fig, ax = plt.subplots()  # Create a figure and an axes.
-      for column in graph_model.y_axes:
-        ax.plot(df[x_axis['heading']], df[column['heading']], label=column['name'])  # Plot some data on the axes.
+      fig, ax = plt.subplots()
+      for column in graph.y_axis['sets']:
+        ax.plot(dataFrame[x_axis['heading']], dataFrame[column['heading']], label=column['name'])  # Plot some data on the axes.
 
-      ax.set_xlabel(graph['x-axis']['name'])  # Add an x-label to the axes.
-      ax.set_ylabel(graph['y-axis']['name'])  # Add a y-label to the axes.
-      ax.set_title(graph_model.title)  # Add a title to the axes.
-      ax.legend()  # Add a legend.
-      if 'show' in graph:
+      ax.set_xlabel(graph.x_axis['name'])
+      ax.set_ylabel(graph.y_axis['name'])
+      ax.set_title(graph.title)
+      ax.legend()
+      if graph.show:
         plt.show()
       fig.set_size_inches(18.5, 10.5)
-      fig.savefig(f"{output_path}{graph_model.filename}.png", bbox_inches='tight')
+      fig.savefig(f"{output_path}{graph.filename}.png", bbox_inches='tight')
