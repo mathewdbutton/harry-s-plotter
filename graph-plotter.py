80ff08f0dea4d133e901f8bd1a5b46c9c6b013b1
Mathew Button <mathewdbutton@gmail.com>
2020-05-03 15:12:40 +1000

    Use yaml to produce graphs

--- graph-plotter.py
+++ graph-plotter.py
@@ -2,21 +2,41 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 import pandas as pd
+import yaml
 
-data = pd.read_csv(r'/Users/mathew/Downloads/129RD_E_1_devc.csv', header=1)
+def load_data(filename, header=1):
+  return pd.read_csv(filename, header=header)
+
+def load_graph_settings(filename="./graph_settings.yml"):
+  with open(filename, 'r') as stream:
+    try:
+        return(yaml.safe_load(stream))
+    except yaml.YAMLError:
+        print(f"Can't read {filename}")
+
+def main():
+    data = load_data(r'/Users/mathew/Downloads/129RD_E_1_devc.csv')
+    settings = load_graph_settings()
+    for graph in settings['graphs']:
+
+      x_axis = graph['x-axis']['sets'][0]
+
+      y_axis_columns = [set['heading'] for set in graph['y-axis']['sets'] ]
+      df = pd.DataFrame(data, columns=[x_axis['heading'], *y_axis_columns])
+
+      # # Note that even in the OO-style, we use `.pyplot.figure` to create the figure.
+      fig, ax = plt.subplots()  # Create a figure and an axes.
+      for column in graph['y-axis']['sets']:
+        ax.plot(df[x_axis['heading']], df[column['heading']], label=column['name'])  # Plot some data on the axes.
+
+      ax.set_xlabel(graph['x-axis']['name'])  # Add an x-label to the axes.
+      ax.set_ylabel(graph['y-axis']['name'])  # Add a y-label to the axes.
+      ax.set_title(graph['name'])  # Add a title to the axes.
+      ax.legend()  # Add a legend.
+      plt.savefig(f"{graph['name']}-foo.png")
+      if 'show' in graph:
+        plt.show()
 
 
-df = pd.DataFrame(data, columns=['Time', 'RAD DOWN_24', 'RAD DOWN_25', 'RAD DOWN_26'])
-
-# # Note that even in the OO-style, we use `.pyplot.figure` to create the figure.
-fig, ax = plt.subplots()  # Create a figure and an axes.
-ax.plot(df["Time"], df["RAD DOWN_24"], label='farts')  # Plot some data on the axes.
-ax.plot(df["Time"], df["RAD DOWN_25"], label='RAD DOWN_25')  # Plot some data on the axes.
-ax.plot(df["Time"], df["RAD DOWN_26"], label='RAD DOWN_26')  # Plot some data on the axes.
-
-ax.set_xlabel('Time in seconds')  # Add an x-label to the axes.
-ax.set_ylabel('RHF')  # Add a y-label to the axes.
-ax.set_title("Things on fire - a plot by harry")  # Add a title to the axes.
-ax.legend()  # Add a legend.
-
-plt.savefig('foo.png')
+if __name__ == "__main__":
+    main()
